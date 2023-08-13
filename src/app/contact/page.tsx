e2e60a4f547e20e8808db59b0c94a89b72c61e63
tfmukles <mukles.themefisher@gmail.com>
2023-08-13 21:35:17 +0600

    state add on form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,52 +1,58 @@
 "use client";
+import { useState } from "react";
 
 const Contact = async () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <section className="section-sm">
         <div className="container">
+          <h1 onClick={() => setOpen(!open)}>ffklad</h1>
           <div className="row">
             <div className="mx-auto md:col-10 lg:col-6">
-              <form name="contact" data-netlify="true" method="post">
-                <input type="hidden" name="form-name" value="contact" />
-                <div className="mb-6">
-                  <input
-                    id="name"
-                    name="user[name]"
-                    className="form-input"
-                    placeholder="John Doe"
-                    type="text"
-                  />
-                </div>
-                <div className="mb-6">
-                  <input
-                    name="user[email]"
-                    className="form-input"
-                    placeholder="john.doe@email.com"
-                    type="email"
-                  />
-                </div>
-                <div className="mb-6">
-                  <textarea
-                    id="message"
-                    name="message"
-                    className="form-input"
-                    placeholder="Message goes here..."
-                    rows={8}
-                  ></textarea>
-                  <input
-                    type="text"
-                    value={"hdfkadkfjklasa"}
-                    name="locaction[country]"
-                  />
-                  <input type="hidden" name="hidden" value={"test"} />
-                  <input type="string" name="string" value={"string"} />
-                </div>
-                <button type="submit" className="btn btn-primary">
-                  Submit
-                </button>
-              </form>
+              {open && (
+                <form name="contact" data-netlify="true" method="post">
+                  <input type="hidden" name="form-name" value="contact" />
+                  <div className="mb-6">
+                    <input
+                      id="name"
+                      name="user[name]"
+                      className="form-input"
+                      placeholder="John Doe"
+                      type="text"
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <input
+                      name="user[email]"
+                      className="form-input"
+                      placeholder="john.doe@email.com"
+                      type="email"
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <textarea
+                      id="message"
+                      name="message"
+                      className="form-input"
+                      placeholder="Message goes here..."
+                      rows={8}
+                    ></textarea>
+                    <input
+                      type="text"
+                      value={"hdfkadkfjklasa"}
+                      name="locaction[country]"
+                    />
+                    <input type="hidden" name="hidden" value={"test"} />
+                    <input type="string" name="string" value={"string"} />
+                  </div>
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </form>
+              )}
             </div>
+            h
           </div>
         </div>
       </section>
