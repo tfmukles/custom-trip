344277a64ac7fc2d750a3db6c6266aeac07f8fc7
tfmukles <mukles.themefisher@gmail.com>
2023-08-13 21:26:28 +0600

    remove getpage lists

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,26 +1,8 @@
 "use client";
 
-import config from "@/config/config.json";
-import { getListPage } from "@/lib/contentParser";
-import PageHeader from "@/partials/PageHeader";
-import SeoMeta from "@/partials/SeoMeta";
-import { RegularPage } from "@/types";
-
 const Contact = async () => {
-  const data: RegularPage = getListPage("contact/_index.md");
-  const { frontmatter } = data;
-  const { title, description, meta_title, image } = frontmatter;
-  const { contact_form_action } = config.params;
-
   return (
     <>
-      <SeoMeta
-        title={title}
-        meta_title={meta_title}
-        description={description}
-        image={image}
-      />
-      <PageHeader title={title} />
       <section className="section-sm">
         <div className="container">
           <div className="row">
