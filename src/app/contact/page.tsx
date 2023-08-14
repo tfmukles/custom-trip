76894fe9cd74c4c5dd8392c3e818c1f428bb096b
tfmukles <mukles.themefisher@gmail.com>
2023-08-14 11:08:40 +0600

    refactor contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,15 +1,24 @@
 "use client";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 const Contact = () => {
   const [open, setOpen] = useState(false);
+  const onSubmitHandler = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <section className="section-sm">
         <div className="container">
           <div className="row">
             <div className="mx-auto md:col-10 lg:col-6">
-              <form name="contact" method="POST" data-netlify="true">
+              <form
+                onSubmit={onSubmitHandler}
+                name="contact"
+                method="POST"
+                data-netlify="true"
+              >
                 <input type="hidden" name="form-name" value="contact" />
                 <p>
                   <label>
